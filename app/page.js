9abce3b333386e4f57da9869764f9b07862f3090
dblok0x <dblok0x@gmail.com>
2024-10-27 15:44:15 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -4,26 +4,22 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center px-20 pt-72 pb-36 md:pb-10 text-xl bg-black text-center white max-md:px-5 max-md:pt-24">
       <article className="flex flex-col items-center max-w-full w-[447px] text-white">
-
-        
- 
-        
         <section className="self-stretch mt-1.5 tracking-widest leading-7 max-md:max-w-full ">
-        <p className="tracking-widest whitespace-pre">+----------------------------------+</p>
+          <p className="tracking-widest whitespace-pre relative  before:absolute before:inset-0 before:animate-typewriter before:bg-black">+----------------------------------+</p>
        
-          <p className="whitespace-pre pt-4">|   $$$ deppterminal TOKEN $$$    |</p>
-          <p className="whitespace-pre pt-2">|   ======================    |</p>
-          <p className="whitespace-pre pt-2">|                                                   |</p>
-          <a href="" className="whitespace-pre pt-2">|        LAUNCH DATE: [NOW]          |</a>
-          <p className="whitespace-pre pt-2">|                                                   |</p>
-          <p className="whitespace-pre pt-2">|      Decentralized Truth Engine     |</p>
-          <p className="whitespace-pre pt-2">|                                                   |</p>
-          <p className="whitespace-pre pt-2">|       Eco-Friendly Blockchains      |</p>
-          <p className="whitespace-pre pt-2">|                                                   |</p>
-          <a href="https://deppterminal.xyz" className="whitespace-pre pt-2">|       www.deppterminal.xyz          |</a>
-          <p className="whitespace-pre pt-2">|                                                   |</p>
-          <p className="whitespace-pre pt-2">|  DISCLAIMER: Invest responsibly |</p>
-          <p className="tracking-widest whitespace-pre pt-4">+----------------------------------+</p>
+          <p className="whitespace-pre pt-4 before:absolute before:inset-0 before:animate-typewriter before:bg-black">|   $$$ deppterminal TOKEN $$$    |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|   ======================    |</p>
+          <p className="whitespace-pre pt-2   before:absolute before:inset-0 before:animate-typewriter before:bg-blacke">|                                                   |</p>
+          <a href="" className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|        LAUNCH DATE: [NOW]          |</a>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|                                                   |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|      Decentralized Truth Engine     |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|                                                   |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|       Eco-Friendly Blockchains      |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|                                                   |</p>
+          <a href="https://deppterminal.xyz" className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|       www.deppterminal.xyz          |</a>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|                                                   |</p>
+          <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|  DISCLAIMER: Invest responsibly |</p>
+          <p className=" tracking-widest whitespace-pre relative  before:absolute before:inset-0 before:animate-typewriter before:bg-black">+----------------------------------+</p>
         </section>
         
         <nav className="flex gap-3.5 items-center mt-80 max-w-full text-base w-[248px] max-md:mt-10">
@@ -43,3 +39,44 @@ export default function Home() {
     </section>
   );
 }
+
+/*
+<section className="flex flex-col items-center px-20 pt-72 pb-36 md:pb-10 text-xl bg-black text-center white max-md:px-5 max-md:pt-24">
+<article className="flex flex-col items-center max-w-full w-[447px] text-white">
+
+  
+
+  
+  <section className="self-stretch mt-1.5 tracking-widest leading-7 max-md:max-w-full ">
+  <p className="tracking-widest whitespace-pre">+----------------------------------+</p>
+ 
+    <p className="whitespace-pre pt-4">|   $$$ deppterminal TOKEN $$$    |</p>
+    <p className="whitespace-pre pt-2">|   ======================    |</p>
+    <p className="whitespace-pre pt-2">|                                                   |</p>
+    <a href="" className="whitespace-pre pt-2">|        LAUNCH DATE: [NOW]          |</a>
+    <p className="whitespace-pre pt-2">|                                                   |</p>
+    <p className="whitespace-pre pt-2">|      Decentralized Truth Engine     |</p>
+    <p className="whitespace-pre pt-2">|                                                   |</p>
+    <p className="whitespace-pre pt-2">|       Eco-Friendly Blockchains      |</p>
+    <p className="whitespace-pre pt-2">|                                                   |</p>
+    <a href="https://deppterminal.xyz" className="whitespace-pre pt-2">|       www.deppterminal.xyz          |</a>
+    <p className="whitespace-pre pt-2">|                                                   |</p>
+    <p className="whitespace-pre pt-2">|  DISCLAIMER: Invest responsibly |</p>
+    <p className="tracking-widest whitespace-pre pt-4">+----------------------------------+</p>
+  </section>
+  
+  <nav className="flex gap-3.5 items-center mt-80 max-w-full text-base w-[248px] max-md:mt-10">
+    <a href="https://x.com/snappfinancer" className="grow self-stretch my-auto hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2">
+      twitter
+    </a>
+    <span aria-hidden="true" className="self-stretch">|</span>
+    <a href="#chart" className="self-stretch my-auto hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2">
+      chart
+    </a>
+    <span aria-hidden="true" className="self-stretch">|</span>
+    <a href="#backrooms" className="self-stretch my-auto hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2">
+      depprooms
+    </a>
+  </nav>
+</article>
+</section>*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        'typewriter': 'typewriter 2s steps(61) forwards',
       },
+      keyframes: {
+        typewriter: {
+          'to': { 
+            left: '100%' 
+          },
+        },
+      },
+      backgroundImage: {},
     },
   },
   plugins: [],
