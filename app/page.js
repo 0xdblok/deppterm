7075df4dcc9fc4c89b71f325461ec6c1fe98495a
dblok0x <dblok0x@gmail.com>
2024-10-28 18:34:49 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center  px-20 pt-72 pb-36 md:pb-10 text-xl bg-black text-center white max-md:px-5 max-md:pt-24">
-      <article className="flex flex-col items-center  text-white">
-        <section className="self-stretch mt-1.5 tracking-widest leading-7 max-md:max-w-full p-6  ">
-          <p className="tracking-widest whitespace-pre relative  before:absolute before:inset-0 before:animate-typewriter before:bg-black">+----------------------------------+</p>
+    <section className="flex flex-col items-center w-screen h-screen  px-20 pt-72 pb-36 md:pb-10 text-xl bg-black text-center white  max-md:pt-24">
+      <article className="flex flex-col items-center  bg-black  text-white">
+        <section className="self-stretch mt-1.5 tracking-widest leading-7 w-screen bg-black max-md:max-w-full  ">
+          <p className="tracking-widest whitespace-pre  before:absolute before:inset-0 before:animate-typewriter before:bg-black">+----------------------------------+</p>
        
           <p className="whitespace-pre pt-4 before:absolute before:inset-0 before:animate-typewriter before:bg-black">|   $$$ deppterminal TOKEN $$$    |</p>
           <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|   ======================    |</p>
@@ -19,7 +19,7 @@ export default function Home() {
           <a href="https://deppterminal.xyz" className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|       www.deppterminal.xyz          |</a>
           <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|                                                   |</p>
           <p className="whitespace-pre pt-2  before:absolute before:inset-0 before:animate-typewriter before:bg-black">|  DISCLAIMER: Invest responsibly |</p>
-          <p className="tracking-widest whitespace-pre relative  before:absolute before:inset-0 before:animate-typewriter before:bg-black pt-4">+----------------------------------+</p>
+          <p className="tracking-widest whitespace-pre   before:absolute before:inset-0 before:animate-typewriter before:bg-black pt-4">+----------------------------------+</p>
 
         </section>
         
